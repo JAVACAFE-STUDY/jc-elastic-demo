045329be7890a8d3f002aab4b78edcdf999acd8d
hrkim <heungrae.kim@navercorp.com>
2019-02-08 18:36:19 +0900

    Sample 수정

--- src/main/java/io/javacafe/demo/api/HelloController.java
+++ src/main/java/io/javacafe/demo/api/HelloController.java
@@ -3,11 +3,16 @@ package io.javacafe.demo.api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
-public class NowController {
+public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
+    	log.info("API가 호출되었습니다.");
+    	
     	return "OK";
     }
 
--- src/main/java/io/javacafe/demo/quartz/job/HelloJob.java
+++ src/main/java/io/javacafe/demo/quartz/job/HelloJob.java
@@ -1,5 +1,7 @@
 package io.javacafe.demo.quartz.job;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,10 +18,23 @@ public class HelloJob implements Job {
 	
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
+
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Date date = new Date();
 		
 		log.info("Hello Job이 실행되었습니다. 실행시간은 {}입니다.", dateFormat.format(date));
+		
+		try {
+			URL url = new URL("http://127.0.0.1");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			
+			int responseCode = con.getResponseCode();
+			log.info("Nginx 호출결과 : {}", responseCode);
+	
+		} catch(Exception e) {
+			log.error(e.getMessage(), e);
+		}
 	}
 
 	
